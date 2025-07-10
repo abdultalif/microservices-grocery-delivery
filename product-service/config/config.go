82f73194f4669b13e719845906afeb74c070794b
abdultalif <abdultalif85@gmail.com>
2025-07-10 18:25:21 +0700

    setup project for product service

---
+++ product-service/config/config.go
@@ -0,0 +1,82 @@
+package config
+
+import "github.com/spf13/viper"
+
+type App struct {
+	AppPort 	string `json:"app_port"`
+	AppEnv  	string `json:"app_env"`
+	
+	JwtSecretKey string `json:"jwt_secret_key"`
+}
+
+type PostgresDB struct {
+	Host      string `json:"host"`
+	Port      string `json:"port"`
+	User      string `json:"user"`
+	Password  string `json:"password"`
+	DBName    string `json:"db_name"`
+	DBMaxOpen int    `json:"db_max_open"`
+	DBMaxIdle int    `json:"db_max_idle"`
+}
+
+type Redis struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
+}
+
+type RabbitMQ struct {
+	Host string `json:"host"`
+	User string `json:"user"`
+	Password string `json:"password"`
+	VirtualHost string `json:"virtual_host"`
+	Port string `json:"port"`
+}
+
+type Supabase struct {
+	URL string `json:"url"`
+	Key string `json:"key"`
+	Bucket string `json:"bucket"`
+}
+
+type Config struct {
+	App      App        `json:"app"`
+	Postgres PostgresDB `json:"postgres"`
+	Redis    Redis      `json:"redis"`
+	RabbitMQ RabbitMQ   `json:"rabbitmq"` 
+	Storage Supabase `json:"supabase"`
+}
+
+func NewConfig() *Config {
+	return &Config{
+		App: App{
+			AppPort: viper.GetString("APP_PORT"),
+			AppEnv: viper.GetString("APP_ENV"),
+			JwtSecretKey: viper.GetString("JWT_SECRET_KEY"),
+		},
+		Postgres: PostgresDB{
+			Host: viper.GetString("DATABASE_HOST"),
+			Port: viper.GetString("DATABASE_PORT"),
+			User: viper.GetString("DATABASE_USER"),
+			Password: viper.GetString("DATABASE_PASSWORD"),
+			DBName: viper.GetString("DATABASE_NAME"),
+			DBMaxOpen: viper.GetInt("DATABASE_MAX_OPEN_CONNECTION"),
+			DBMaxIdle: viper.GetInt("DATABASE_MAX_IDLE_CONNECTION"),
+		},
+		Redis: Redis{
+			Host: viper.GetString("REDIS_HOST"),
+			Port: viper.GetString("REDIS_PORT"),
+		},
+		RabbitMQ: RabbitMQ{
+			Host: viper.GetString("RABBITMQ_HOST"),
+			Port: viper.GetString("RABBITMQ_PORT"),
+			User: viper.GetString("RABBITMQ_USER"),
+			Password: viper.GetString("RABBITMQ_PASSWORD"),	
+			VirtualHost: viper.GetString("RABBITMQ_VIRTUAL_HOST"),
+		},
+		Storage: Supabase{
+			URL: viper.GetString("SUPABASE_STORAGE_URL"),
+			Key: viper.GetString("SUPABASE_STORAGE_KEY"),
+			Bucket: viper.GetString("SUPABASE_STORAGE_BUCKET"),
+		},
+	}
+}
---
+++ product-service/config/database.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"fmt"
+	// "user-service/database/seeds"
+
+	"github.com/rs/zerolog/log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Postgres struct {
+	DB *gorm.DB
+}
+
+func (cfg Config) ConnectionPostgres() (*Postgres, error) {
+	dbConnString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", 
+		cfg.Postgres.Host,
+		cfg.Postgres.Port,
+		cfg.Postgres.User,
+		cfg.Postgres.Password,
+		cfg.Postgres.DBName)	
+
+	db, err := gorm.Open(postgres.Open(dbConnString), &gorm.Config{})
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to connect to database")
+		return nil, err
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to connect to database")
+		return nil, err
+	}
+
+	// seeds.SeedRoles(db)
+	// seeds.SeedAdmin(db)
+
+
+	sqlDB.SetMaxOpenConns(cfg.Postgres.DBMaxOpen)
+	sqlDB.SetMaxIdleConns(cfg.Postgres.DBMaxIdle)
+
+	return &Postgres{
+		DB: db,
+	}, nil
+}
