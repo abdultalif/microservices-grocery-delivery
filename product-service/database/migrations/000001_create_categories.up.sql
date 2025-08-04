ad35e4e1dcfca2f845bbed71b35b472e43ebd8ad
abdultalif <abdultalif85@gmail.com>
2025-08-04 23:37:36 +0700

    chore: update field status category

--- product-service/database/migrations/000001_create_categories.up.sql
+++ product-service/database/migrations/000001_create_categories.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS categories (
     parent_id UUID NULL REFERENCES categories(id),
     name VARCHAR(255) NOT NULL,
     icon VARCHAR(255) NOT NULL,
-    status BOOLEAN DEFAULT TRUE,
+    status VARCHAR(255) NOT NULL,
     slug VARCHAR(255) UNIQUE NOT NULL,
     description TEXT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
